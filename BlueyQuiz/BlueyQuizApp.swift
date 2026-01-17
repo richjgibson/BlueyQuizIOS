c33f2e1d15809e361198383c0ac34e67ecffeb65
Rich Gibson <richgibson86@gmail.com>
2026-01-17 23:21:34 +0000

    Initial Commit

---
+++ BlueyQuiz/BlueyQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueyQuizApp.swift
+//  BlueyQuiz
+//
+//  Created by Rich on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BlueyQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
