8a7bac4672a212192a70191fb9ab184544e57e4e
Rich Gibson <richgibson86@gmail.com>
2026-01-18 00:27:37 +0000

    Working UI

--- BlueyQuiz/ContentView.swift
+++ BlueyQuiz/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var episodeList = []
+    @State private var correctAnswer = Int.random(in: 0..<3)
+    @State private var playerScore: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(stops: [Gradient.Stop(color: .blue, location: 0.35), Gradient.Stop(color: .orange, location: 0.55)], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                Text("Bluey Episode Quiz")
+                    .font(.largeTitle.bold())
+                
+                Spacer()
+                
+                VStack(spacing: 30){
+                    VStack {
+                        Text("Tap the scene\n from episode")
+                        Text("\(correctAnswer)")
+                    }
+                    
+                    VStack(spacing: 15) {
+                        ForEach(0..<3) { number in
+                            Button {
+                                print("Button \(number) tapped")
+                            } label: {
+                                Text("BUTTON")
+                            }
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding(20)
+                
+                Spacer()
+                
+                Text("Score: \(playerScore)")
+                    .font(.largeTitle.bold())
+            }
         }
-        .padding()
     }
 }
 
